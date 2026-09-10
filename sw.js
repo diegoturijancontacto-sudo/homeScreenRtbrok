76df67bd5d060a958db8a4c6bbd891098eaf0efa
Diego Turijan Gerver <diego.turijan.contacto@gmail.com>
2026-09-09 22:35:15 -0600

    Update cache version to v8 in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mi-pwa-cache-v7'; // 1. Cambia el nombre de la versión
+const CACHE_NAME = 'mi-pwa-cache-v8'; // 1. Cambia el nombre de la versión
 const urlsToCache = [
   './',
   './mobile.html',
